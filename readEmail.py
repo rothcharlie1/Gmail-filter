bc9fc3a3e989c4c4fec24f1a322b40fec724dd5d
rothcharlie1 <43792220+rothcharlie1@users.noreply.github.com>
2019-11-07 16:48:16 -0500

    Update readEmail.py

--- readEmail.py
+++ readEmail.py
@@ -16,42 +16,41 @@ def importer():
     store = file.Storage('token.json')
     creds = store.get()
     if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('client_secret_44702673440-vq3k3r554g4oh4lod094kl1okbmga5c7.apps.googleusercontent.com.json', SCOPES)
+        flow = client.flow_from_clientsecrets('client_secret_44702673440-vq3k3r554g4oh4lod094kl1okbmga5c7.apps.googleusercontent.com.json', SCOPES)  #i think this part establishes the connection between the program and the email and does authorization
         creds = tools.run_flow(flow, store)
     service = build('gmail', 'v1', http=creds.authorize(Http()))
-    
-    # Call the Gmail API to fetch INBOX
-    results = service.users().messages().list(userId='me',labelIds = ['INBOX']).execute()
+     # Call the Gmail API to fetch INBOX
+    results = service.users().messages().list(userId='me',labelIds = ['INBOX']).execute()  #fetches messages
     messages = results.get('messages', [])
-    
-    idList = []
-    
+
+    idList = []  
     if not messages:
         print ("No messages found.")
     else:
         for message in messages:
-            msg = service.users().messages().get(userId='me', id=message['id']).execute()
+            msg = service.users().messages().get(userId='me', id=message['id']).execute()  #loop through all messages, pull out message, add id to the list of ids, and run the content filter
             idList.append(message['id'])
             content_filter(msg)
   
-def find_index(lst, key, value):
+def find_index(lst, key, value): #this method finds a specific value for a specific key in a list and returns the index of it
     for i, dic in enumerate(lst):
         if dic[key] == value:
             return i
     return None
 
 def content_filter(mesg):    
-    separated_list = mesg['payload']['headers']
+    separated_list = mesg['payload']['headers'] #location of list of headers
     
-    from_index = find_index(separated_list, 'name', 'From')
-    from_val = mesg['payload']['headers'][from_index]['value']
+    from_index = find_index(separated_list, 'name', 'From') 
+    from_val = mesg['payload']['headers'][from_index]['value'] #gets where mail is from
 
-    subject_index = find_index(separated_list, 'name', 'Subject')
+    subject_index = find_index(separated_list, 'name', 'Subject') #gets subject
     subject_val = mesg['payload']['headers'][subject_index]['value']
 
-    snippet_val = mesg['snippet']
+    snippet_val = mesg['snippet'] #gets snippet
     
-    print(from_val)
+    print(from_val) #outputs the 3 parameters of each email
+    print(subject_val)
     print(snippet_val)
     print()
 
