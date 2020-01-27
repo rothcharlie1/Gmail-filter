bbdad5b85d0b426ca3b846cde06afffd65991676
rothcharlie1 <43792220+rothcharlie1@users.noreply.github.com>
2020-01-26 19:35:23 -0500

    blah

--- readEmail.py
+++ readEmail.py
@@ -49,10 +49,10 @@ def content_filter(mesg):
 
     snippet_val = mesg['snippet'] #gets snippet
     
-    # print(from_val) #outputs the 3 parameters of each email
-    # print(subject_val)
-    # print(snippet_val)
-    # print()
+    print(from_val) #outputs the 3 parameters of each email
+    print(subject_val)
+    print(snippet_val)
+    print()
 
 def ModifyMessage(service, user_id, msg_id, msg_labels):
   """Modify the Labels on the given Message.
@@ -85,50 +85,7 @@ def CreateMsgLabels():
     A label update object.
   """
   return {'removeLabelIds': [], 'addLabelIds': ['UNREAD', 'INBOX', 'Label_2']}
-
-
-    
-    
-   
-# def create_message(sender, to, subject, message_text):
-#   """Create a message for an email.
-
-#   Args:
-#     sender: Email address of the sender.
-#     to: Email address of the receiver.
-#     subject: The subject of the email message.
-#     message_text: The text of the email message.
-
-#   Returns:
-#     An object containing a base64url encoded email object.
-#   """
-#   message = MIMEText(message_text)
-#   message['to'] = to
-#   message['from'] = sender
-#   message['subject'] = subject
-#   return {'raw': base64.urlsafe_b64encode(message.as_string())}
-
-# def send_message(service, user_id, message):
-#   """Send an email message.
-
-#   Args:
-#     service: Authorized Gmail API service instance.
-#     user_id: User's email address. The special value "me"
-#     can be used to indicate the authenticated user.
-#     message: Message to be sent.
-
-#   Returns:
-#     Sent Message.
-#   """
-#   try:
-#     message = (service.users().messages().send(userId=user_id, body=message)
-#                .execute())
-#     print ('Message Id: %s' % message['id'])
-#     return message
-#   except errors.HttpError as error:
-#     print (f'An error occurred: {error}')
-#     return None
-   
+ 
             
 if __name__ == '__main__':
     main()
