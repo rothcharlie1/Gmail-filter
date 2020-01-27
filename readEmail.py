2ba9e590d445abae32559b17be2f2e6fbf8c04fd
rothcharlie1 <43792220+rothcharlie1@users.noreply.github.com>
2020-01-26 19:57:41 -0500

    added this code to help matt

--- readEmail.py
+++ readEmail.py
@@ -85,6 +85,43 @@ def CreateMsgLabels():
     A label update object.
   """
   return {'removeLabelIds': [], 'addLabelIds': ['UNREAD', 'INBOX', 'Label_2']}
+
+def CreateLabel(service, user_id, label_object):
+  """Creates a new label within user's mailbox, also prints Label ID.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    label_object: label to be added.
+
+  Returns:
+    Created Label.
+  """
+  try:
+    label = service.users().labels().create(userId=user_id, body=label_object).execute()
+    print(label['id'])
+    return label
+  except errors.HttpError as error:
+    print 'An error occurred: %s' % error
+
+
+def MakeLabel(label_name, mlv='show', llv='labelShow'):
+  """Create Label object.
+
+  Args:
+    label_name: The name of the Label.
+    mlv: Message list visibility, show/hide.
+    llv: Label list visibility, labelShow/labelHide.
+
+  Returns:
+    Created Label.
+  """
+  label = {'messageListVisibility': mlv,
+           'name': label_name,
+           'labelListVisibility': llv}
+  return label
+
  
             
 if __name__ == '__main__':
